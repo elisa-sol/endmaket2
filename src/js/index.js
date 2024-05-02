b871ce5db00df3c57a1f8ad85af5a304a3d92a58
elisa-sol <elizaveta-2002@bk.ru>
2024-05-02 19:14:12 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
+import '../scss/base/fonts.scss'
 import '../scss/style.scss'
+
+let init = false;
+let swiper;
+
+function swiperCard() {
+  if (window.innerWidth < 768) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper", {
+        loop: true,
+        speed: 1000,
+        freeMode: true,
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+swiperCard();
+window.addEventListener("resize", swiperCard);
 
 console.log('Works!')
