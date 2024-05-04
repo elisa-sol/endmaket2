f96b7e2527b23cb11b2af9c023d9e5db17d5a95a
elisa-sol <elizaveta-2002@bk.ru>
2024-05-04 20:36:25 +0300

    commit

--- src/js/scripts/sidebar.js
+++ src/js/scripts/sidebar.js
@@ -5,6 +5,7 @@ let burgerButton = document.querySelector('.upperMenu__burger');
 let hideHeader = document.querySelector('.header');
 let crossButton = document.querySelector('.sideBar__cross');
 let group = document.querySelector('.upperMenu__group');
+let navigation = document.querySelector('.navigation');
 let width = window.innerWidth;
 
 function checkWidth() {
@@ -27,14 +28,14 @@ function checkWidth() {
   } else if (width >= 768 && width <= 1440) {
     burgerButton.addEventListener('click', function() {
       if (openSideBar && closeMain) {
-        // burgerButton.style.display = 'none';
-        // group.style.display = 'none';
-        // hideHeader.style.position = 'absolute';
-        // hideHeader.style.right = '20px';
+        burgerButton.style.display = 'none';
+        group.style.display = 'none';
+        hideHeader.style.position = 'absolute';
+        hideHeader.style.right = '20px';
 
         openSideBar.style.display = 'flex';
         hideHeader.style.display = 'none';
-        // hideHeader.style.display = 'flex';
+        hideHeader.style.display = 'flex';
         hideHeader.style.opacity = '0.08';
         closeMain.style.opacity = '0.08';
       }
@@ -42,17 +43,27 @@ function checkWidth() {
 
     crossButton.addEventListener('click', function() {
       if (openSideBar && closeMain) {
-        // burgerButton.style.display = 'flex';
-        // group.style.display = 'flex';
-        // hideHeader.style.right = '0';
+        burgerButton.style.display = 'flex';
+        group.style.display = 'flex';
+        hideHeader.style.right = '0';
 
         openSideBar.style.display = 'none';
-        // hideHeader.style.position = 'sticky';
+        hideHeader.style.position = 'sticky';
         hideHeader.style.opacity = '1';
         hideHeader.style.display = 'block';
         closeMain.style.opacity = '1';
       }
     });
+  }
+  else {
+    closeMain.style.marginLeft = '320px';
+    navigation.style.position = 'relative';
+    openSideBar.style.position = 'absolute';
+    openSideBar.style.top = '0';
+    openSideBar.style.left = '0';
+    openSideBar.style.bottom = '0';
+    openSideBar.style.zIndex = '1000';
+    openSideBar.style.display = 'flex';
   }
 }
 
